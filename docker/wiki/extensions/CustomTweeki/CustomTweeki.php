f0246c9471bd03f4b9194843af5b217761bc0bf5
Monnier <monnierant42@gmail.com>
2018-12-21 14:14:47 +0100

    remove extensions

--- docker/wiki/extensions/CustomTweeki/CustomTweeki.php
+++
@@ -1,9 +0,0 @@
-$wgResourceModules['x.CUSTOMTWEEKI.styles'] = array(
-  'styles' => array(
-		'screen.css' => array( 'media' => 'screen' )
-	),
-  'localBasePath' => __DIR__,
-  'remoteExtPath' => 'CUSTOMTWEEKI',
-);
-
-$wgTweekiSkinCustomCSS[] = 'x.CUSTOMTWEEKI.styles';
